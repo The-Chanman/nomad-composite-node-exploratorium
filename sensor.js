32edd3937171558cbe9398d7c112b900766b57b0
The-Chanman <ecphylo@gmail.com>
2016-12-05 00:07:08 -0800

    composite node with data maintainer class

--- sensor.js
+++ sensor.js
@@ -1,58 +1,114 @@
 const Nomad = require('nomad-stream')
 const moment = require('moment')
+
 const nomad = new Nomad()
-const fetch = require('node-fetch')
 
 let instance = null
-const frequency = 60 * 60 * 1000
+const timeThreshold = 3 * 60 * 60 * 1000 // 3 hours
+const frequency = 60 * 60 * 1000 // 1 hour
+let lastPub = null
 
-// parse into url object 
-let base = 'http://erddap.exploratorium.edu:8080/erddap/tabledap/explorebeaconbay5min.json?time,Temperature_1,Air_Pressure,Temperature_2,Relative_Humidity,Dew_Point,O3_1,CO_1,NO2_1,O3_Working_2,O3_Auxiliary_2,CO_Working_2,CO_Auxiliary_2,NO_Working,NO_Auxiliary,NO_Auxiliary_2,NO2_Working_2,NO2_Auxiliary_2,Particulate_high,Particulate_total,Particulate_pct,CO2,Temperature_3,station_id,latitude,longitude&time>='
-
-function buildUrl() {
-  const d = new moment()
-  const twohourAgo = d.subtract(2, 'h')
-  const timeString = twohourAgo.toISOString()
-  const url = `${base}${timeString}`
-  const encoded = encodeURI(url)
-  return encoded
+const defaultPublishData = {
+  bayWaterSensor: {
+    salinity: {
+      data: "",
+      units: "",
+      time: "",
+    }
+  },
+  baySideBeaconSensor: {
+    Dew_Point: {
+      data: "",
+      units: "",
+      time: "",
+    }
+  }
 }
 
-function transform(json) {
-  let transformed = {}
-  transformed['columnNames'] = json.table.columnNames
-  transformed['columnTypes'] = json.table.columnTypes
-  transformed['columnUnits'] = json.table.columnUnits
-  transformed['data'] = json.table.rows[0]
-  return transformed
+class DataMaintainer {
+  constructor(){
+    this.data = defaultPublishData
+  }
+  setValue(key, value){
+    let cleanedKey = this.cleanKey(key)
+    if(cleanedKey in this.data){
+      this.data[cleanedKey].data = value.data
+      this.data[cleanedKey].time = value.time
+      this.data[cleanedKey].units = value.units
+    } else {
+      this.data[cleanedKey] = value
+    }
+  }
+  cleanKey(key){
+    let cleanedKey = key.replace(/\s+/, '\x01').split('\x01')[0]
+    cleanedKey = cleanedKey.toLowerCase()
+    return cleanedKey
+  }
+  getAll(){
+    return this.data
+  }
+  isAllFilled(){
+    return this.data["bayWaterSensor"]["salinity"]["data"] && this.data["bayWaterSensor"]["salinity"]["time"] && this.data["baySideBeaconSensor"]["Dew_Point"]["data"] && this.data["baySideBeaconSensor"]["Dew_Point"]["time"]
+  }
+  clear(){
+    this.data = defaultPublishData
+  }
+  toString(){
+    return JSON.stringify(this.data)
+  }
 }
 
-// returns promise
-function getMessage() {
-  const url = buildUrl()
-  return fetch(url).then(res => {
-  	console.log(res.body)
-    return res.json();
-  }).then(json => {
-    const formatted = transform(json)
-    return Promise.resolve(JSON.stringify(formatted))
-  }).catch(err => {
-    console.log(err)
-    return Promise.reject(err)
+function getTime() {
+  return new moment()
+}
+
+let dataManager = new DataMaintainer()
+
+nomad.prepareToPublish()
+  .then((n) =>{
+    instance = n
+    return instance.publishRoot('hello this is a composite node for the exploratorium')
   })
-}
+  .then(() =>{
+    lastPub = getTime()
+    nomad.subscribe(['QmdmSfLYyyKRdadFPbYv8o89ibU4tpcdFve6dqQwbQ9QhU', 'Qmcd8r4J81uxcLSsrUGMh9D4Rh47UYmPNDCEpuifxuNyNy'], function(message) {
+      console.log(message.message)
+      messageData = JSON.parse(message.message)
+      try{
+        dataManager.setValue(messageData.columnNames[5], {data: messageData.data[5], time: messageData.data[0]})
+      }
+      catch(err){
+        console.log("DataMaintainer failed with error of " + err)
+      }
+      console.log(dataManager.toString())
+      let currentTime = getTime()
+      let timeSince = currentTime - lastPub
+      if (timeSince >= frequency){
+        console.log("timeSince >= timeBetween")
+        if (dataManager.isAllFilled){
+          // publish if everything is full
+          console.log("***************************************************************************************")
+          console.log(dataManager.getAll())
+          console.log("***************************************************************************************")
 
-nomad.prepareToPublish().then((n) => {
-  instance = n
-  return instance.publishRoot('hello')
-}).then(() => {
-  setInterval(() => {
-    getMessage().then(m => return instance.publish(m))
-    .catch(err => {
-      console.log(`Error: ${err}`)
+          instance.publish(dataManager.toString())
+            .catch(err => console.log(`Error: ${JSON.stringify(err)}`))
+          dataManager.clear()  
+          lastPub = currentTime
+        }
+      }
+      if (timeSince >= timeThreshold){
+        // publish what we got
+        instance.publish(dataManager.toString())
+          .catch(err => console.log(`Error: ${JSON.stringify(err)}`))
+        console.log("***************************************************************************************")
+        console.log(dataManager.getAll())
+        console.log("***************************************************************************************")
+        dataManager.clear()  
+        lastPub = currentTime
+      }
     })
-  }, frequency)  
-})
- .catch(console.log)
+  })
+  .catch(err => console.log(`Error in main loop: ${JSON.stringify(err)}`))
 
 
